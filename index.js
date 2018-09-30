9070f41e1ad1de0c554e7e2d13ca4337a54fefbb
Raymond Yeh <yehray@gmail.com>
2018-09-30 06:03:32 -0700

    bot responds to channel message was sent from

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const bot = new SlackBot({
 
 // Start Handler
 bot.on('start', () => {
-  bot.postMessageToChannel('general', 
+  bot.postMessage('general', 
   'Type in the city to find out the weather',
   );
   botID = bot.self.id;
@@ -34,11 +34,13 @@ bot.on('message', data => {
     return;
   }
   var str = data.text;
-  handleMessage(str.replace(/\s*\<.*?\>\s*/g, ''));
+  var channel = data.channel;
+  console.log(data);
+  handleMessage(str.replace(/\s*\<.*?\>\s*/g, ''), channel);
 });
 
 // Responds to Data
-function handleMessage(message){
+function handleMessage(message, channel){
   var url = "https://api.openweathermap.org/data/2.5/weather?q=" + message + "&appid=" + openweathermap_api_key + "&units=imperial";
   console.log(url);
   fetch(url).then((response) => {
@@ -50,8 +52,8 @@ function handleMessage(message){
     }
   })
   .then((responseJson) => {
-    bot.postMessageToChannel(
-      'general',
+    bot.postMessage(
+      channel,
       'Here are the weather conditions in ' + message  + ':\n' +
       'The current condition is: ' + responseJson.weather[0].description + '\n' +
       'The current temperature is: ' + responseJson.main.temp + '°F\n' +
@@ -63,10 +65,12 @@ function handleMessage(message){
   })
   .catch((error) => {
     console.log(error)
-    bot.postMessageToChannel(
-      'general',
+    bot.postMessage(
+      channel,
       'Could not find the weather at the city entered. Please try another name of a city.'
     );
   });
 } 
+
+
 
